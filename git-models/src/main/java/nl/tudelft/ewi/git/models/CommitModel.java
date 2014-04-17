e0d2b294358d5cb355b3a0322a3781c422dc686a
Michael de Jong <michaelj@minicom.nl>
2014-04-17 13:23:18 +0200

    Allow individual commits to be retrieved through the API.

--- git-models/src/main/java/nl/tudelft/ewi/git/models/CommitModel.java
+++ git-models/src/main/java/nl/tudelft/ewi/git/models/CommitModel.java
@@ -17,12 +17,12 @@ public class CommitModel {
 	private String author;
 	private long time;
 	private String message;
-	
+
 	public void setAuthor(String name, String emailAddress) {
 		if (Strings.isNullOrEmpty(emailAddress)) {
 			setAuthor(name);
 		}
 		setAuthor(name + " <" + emailAddress + ">");
 	}
-	
+
 }
