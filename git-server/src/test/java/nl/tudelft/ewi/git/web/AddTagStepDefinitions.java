c6fa0042a41120518c7f8fdadee25e2033773e58
Maaike Visser <m.e.b.p.visser@student.tudelft.nl>
2017-06-21 16:47:08 +0200

    Add cucumber step to check that a tag has been added

--- git-server/src/test/java/nl/tudelft/ewi/git/web/AddTagStepDefinitions.java
+++ git-server/src/test/java/nl/tudelft/ewi/git/web/AddTagStepDefinitions.java
@@ -3,6 +3,9 @@ package nl.tudelft.ewi.git.web;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import cucumber.runtime.java.guice.ScenarioScoped;
+import lombok.Getter;
 import lombok.val;
 import nl.tudelft.ewi.git.models.CommitModel;
 import nl.tudelft.ewi.git.models.DetailedCommitModel;
@@ -13,13 +16,24 @@ import nl.tudelft.ewi.git.web.api.RepositoriesApiImpl;
 import nl.tudelft.ewi.git.web.api.RepositoryApiImpl;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.hamcrest.Matchers;
 
 import javax.inject.Inject;
 import javax.xml.soap.Detail;
 
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by Sayra Ranjha and Maaike Visser on 21-6-2017.
  */
+@ScenarioScoped
 public class AddTagStepDefinitions {
 
     public static final String DEFAULT_COMMIT_AUTHOR_NAME = "SayraMaaike";
@@ -30,33 +44,38 @@ public class AddTagStepDefinitions {
     @Inject
     private RepositoriesApiImpl repositoriesApi;
 
+    @Getter
+    private TagModel tagModel;
+
     @When("^I tag the commit$")
     public void iTagTheCommit() throws Throwable {
-
-
-//        CommitApi commitApi = repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName()).getCommit(cloneStepDefinitions.getCreatedCommitId());
-//        CommitModel commitModel = commitApi.get();
-
-        String commitId = repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName())
+        String commitId = repositoriesApi
+                .getRepository(cloneStepDefinitions.getRepositoryName())
                 .getBranch("master")
                 .getCommit()
                 .get()
                 .getCommit();
 
-        CommitModel commitModel = repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName()).getCommit(commitId).get();
+        CommitModel commitModel = repositoriesApi
+                .getRepository(cloneStepDefinitions.getRepositoryName())
+                .getCommit(commitId)
+                .get();
 
-        TagModel tagModel = new TagModel();
+        tagModel = new TagModel();
         tagModel.setName("ourtag");
         tagModel.setCommit(commitModel);
         tagModel.setDescription("This is our tag.");
 
-        repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName()).addTag(tagModel);
+        repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName())
+                .addTag(tagModel);
     }
 
     @Then("^A tag is added to the commit$")
     public void aTagIsAddedToTheCommit() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+        Collection<TagModel> tags = repositoriesApi.getRepository(cloneStepDefinitions.getRepositoryName()).getTags();
 
+        assertThat(tags, contains(
+                hasProperty("name", containsString(tagModel.getName()))));
+
+    }
 }
